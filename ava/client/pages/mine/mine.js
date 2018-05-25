202ad848203850445b580bcb60a1df937246609c
Priscilla <33865934+Prisicilla@users.noreply.github.com>
2018-05-25 10:58:03 +0800

    Create mine.js

---
+++ ava/client/pages/mine/mine.js
@@ -0,0 +1,80 @@
+var app = getApp()
+
+Page({
+  data: {
+    userInfo: {},
+    canIUse: wx.canIUse('button.open-type.getUserInfo')
+
+  },
+  onLoad: function (options) {
+    // 生命周期函数--监听页面加载   
+    console.log('onLoad')
+    var that = this
+    //调用应用实例的方法获取全局数据
+    wx.getUserInfo(function (userInfo) {
+      //更新数据
+      that.setData({
+        userInfo: userInfo
+      })
+    })
+    
+  },
+  onShareAppMessage: function () {
+    // 用户点击右上角分享
+    return {
+      title: '', // 分享标题
+      desc: '', // 分享描述
+      path: '' // 分享路径
+    }
+  },
+  showMyWord: function () {
+    wx.showModal({
+      title: '提示',
+      content: '此功能暂未开放，敬请期待！',
+      showCancel: false,
+      success: function (res) {
+        if (res.confirm) {
+          console.log('用户点击确定')
+        }
+      }
+    })
+  },
+  showClause: function () {
+    wx.navigateTo({
+      url: '../linkCgi/link',
+      success: function (res) {
+        // success
+      },
+      fail: function () {
+        // fail
+      },
+      complete: function () {
+        // complete
+      }
+    })
+  },
+
+  getUserInfo: function (e) {
+    console.log(e)
+    app.globalData.userInfo = e.detail.userInfo
+    this.setData({
+      userInfo: e.detail.userInfo,
+      hasUserInfo: true
+    })
+  },
+
+  showFeedback: function () {
+    wx.navigateTo({
+      url: '../addCgi/addCgi',
+      success: function (res) {
+        // success
+      },
+      fail: function () {
+        // fail
+      },
+      complete: function () {
+        // complete
+      }
+    })
+  }
+})
